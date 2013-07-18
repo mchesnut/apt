7de4b333900f473acfcc62355f4a5105cfbf1ffc
Mike Chesnut <chewie@crittercism.com>
2013-07-17 23:03:26 -0700

    adding DEBUG labels to more easily search for my changes

--- providers/repository.rb
+++ providers/repository.rb
@@ -89,10 +89,10 @@ action :add do
   new_resource.updated_by_last_action(false)
   @repo_file = nil
 
-  Chef::Log.info "repo_file starts out as: '" + @repo_file.to_s + "'"
+  Chef::Log.info "DEBUG repo_file starts out as: '" + @repo_file.to_s + "'"
 
   recipe_eval do
-    Chef::Log.info "entering recipe_eval block"
+    Chef::Log.info "DEBUG entering recipe_eval block"
     # add key
     if new_resource.keyserver && new_resource.key
       install_key_from_keyserver(new_resource.key, new_resource.keyserver)
@@ -110,14 +110,14 @@ action :add do
     end
 
     # build repo file
-    Chef::Log.info "building repository with uri \"#{new_resource.uri}\", distribution \"#{new_resource.distribution}\", components \"#{new_resource.components}\", arch \"#{new_resource.arch}\", deb_src \"#{new_resource.deb_src}\""
+    Chef::Log.info "DEBUG building repository with uri \"#{new_resource.uri}\", distribution \"#{new_resource.distribution}\", components \"#{new_resource.components}\", arch \"#{new_resource.arch}\", deb_src \"#{new_resource.deb_src}\""
     repository = build_repo(new_resource.uri,
                             new_resource.distribution,
                             new_resource.components,
                             new_resource.arch,
                             new_resource.deb_src)
 
-    Chef::Log.info "building repo_file with name \"#{new_resource.name}.list\""
+    Chef::Log.info "DEBUG building repo_file with name \"#{new_resource.name}.list\""
     @repo_file = file "/etc/apt/sources.list.d/#{new_resource.name}.list" do
       owner "root"
       group "root"
@@ -129,7 +129,7 @@ action :add do
     end
   end
 
-  Chef::Log.info "repo_file looks like: '" + @repo_file.to_s + "'"
+  Chef::Log.info "DEBUG repo_file looks like: '" + @repo_file.to_s + "'"
   raise RuntimeError, "The repository file to create is nil, cannot continue." if @repo_file.nil?
   new_resource.updated_by_last_action(@repo_file.updated?)
 end
